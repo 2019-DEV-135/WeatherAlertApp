ab0aca5bdc2e7e553cf11356e1057320fabfb799
Umar Yaqub <yaqub_umar@yahoo.co.uk>
2019-02-05 14:25:00 +0000

    Initial Commit

---
+++ Weather Alert App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Alert App
+//
+//  Created by Umar Yaqub on 05/02/2019.
+//  Copyright © 2019 Umar Yaqub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
